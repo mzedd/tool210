ea50135b3192e3eb9ffc9a205411f1ee4589dacd
Max Zimmermann <mz.max.zimmermann@gmail.com>
2020-08-27 08:52:15 +0200

    Spawn ClipScreen again as DockWidget

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,7 +1,13 @@
 #include "MainWindow.h"
+#include <QDockWidget>
+#include "modules/ClipScreen/ClipScreen.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    QDockWidget *widget = new QDockWidget(QString("Screen"), this);
+    widget->setWidget(new ClipScreen);
+    addDockWidget(Qt::DockWidgetArea::LeftDockWidgetArea, widget);
 
+    show();
 }
