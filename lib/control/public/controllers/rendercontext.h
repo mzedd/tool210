193a456f5525257c558e67ca4918894f1a2f4b36
Max Zimmermann <mz.max.zimmermann@gmail.com>
2020-09-28 09:13:58 +0200

    Added at rendercontext class for time managemet

---
+++ lib/control/public/controllers/rendercontext.h
@@ -0,0 +1,33 @@
+#ifndef RENDERCONTEXT_H
+#define RENDERCONTEXT_H
+
+#include <QObject>
+#include <QElapsedTimer>
+
+class RenderContext : public QObject
+{
+    Q_OBJECT
+
+public:
+    RenderContext(QObject *parent = nullptr);
+
+    float time() const;
+    void setTime(float time);
+
+    bool run() const;
+    void setRun(bool run);
+
+    float deltaTime();
+
+private:
+    float time_;
+    bool run_;
+
+    QElapsedTimer timer;
+    float timeAtRunChanged;
+
+Q_SIGNALS:
+    void runChanged();
+};
+
+#endif // RENDERCONTEXT_H
---
+++ lib/control/source/controllers/rendercontext.cpp
@@ -0,0 +1,41 @@
+#include "rendercontext.h"
+#include <algorithm>
+
+constexpr float MSEC_PER_SECS = 0.001f;
+
+RenderContext::RenderContext(QObject *parent) :
+    QObject(parent),
+    time_(10.0f),
+    timeAtRunChanged(0.0f)
+{
+    timer.start();
+}
+
+float RenderContext::time() const
+{
+    return time_;
+}
+
+void RenderContext::setTime(float time)
+{
+    time_ = std::min(0.0f, time);
+    timeAtRunChanged = timer.elapsed() * MSEC_PER_SECS - time_;
+}
+
+bool RenderContext::run() const
+{
+    return run_;
+}
+
+void RenderContext::setRun(bool run)
+{
+    run_ = run;
+    timeAtRunChanged = timer.elapsed() * MSEC_PER_SECS - time_;
+    emit runChanged();
+}
+
+float RenderContext::deltaTime()
+{
+    time_ = timer.elapsed() * MSEC_PER_SECS - timeAtRunChanged;
+    return time_;
+}
